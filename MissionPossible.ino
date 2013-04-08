5e80623761918118dfe40833b047fafa676c0218
Gerrit Niezen <gerrit.niezen@gmail.com>
2013-04-08 11:16:49 +0100

    Now works with 10 cars travelling at different velocities

--- MissionPossible.ino
+++ MissionPossible.ino
@@ -2,23 +2,35 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
+//Ethernet settings
 byte mac[] = {  0x90, 0xA2, 0xDA, 0x0D, 0xA6, 0xA5 };
 IPAddress server(173,194,34,102); // Change this to server address
-
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, 6, NEO_GRB + NEO_KHZ800);
 EthernetClient client;
 
-long previousMillis = 0;
-long interval = 50; //delay in ms
-uint16_t i = 0;
+//LED strip settings
+uint16_t stripLength = 240;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(stripLength, 6, NEO_GRB + NEO_KHZ800);
 
-uint32_t color = Wheel(random(255));
+// Delay variables
+long previousMillis = 0;
+long interval = 20; //delay between updates
+uint16_t t = 0; //time step
+
+// Set up cars
+uint8_t nrCars = 10;
+uint16_t currentPos[] = {0,0,0,0,0,0,0,0,0,0};
+uint32_t color[] = {0,0,0,0,0,0,0,0,0,0};
+uint8_t velocity[] = {1,2,3,4,5,6,7,8,9,10};
 
 
 void setup() {
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
   randomSeed(analogRead(0));
+  
+  //Initialise car colours
+  for(int i=0; i<nrCars;i++)
+    color[i] = Wheel(random(255));
   
   /*
   Serial.begin(9600); //Wait for serial monitor
@@ -69,53 +81,70 @@ void loop() {
   
   if(currentMillis - previousMillis > interval) {
     
-    previousMillis = currentMillis; // save the last time you blinked the LED 
+    previousMillis = currentMillis; // save the last time we updated the strip
     
-    if(i==0) {
-      //startup
-      color = Wheel(random(255));
-      car(color,length,0);
+    for(int nr = 0; nr < nrCars; nr++) {
+    
+      if(currentPos[nr] == stripLength) {
+        //Restart car
+        currentPos[nr] = 0;
+        car(color[nr],length,0);
+      }       
     }
     
-    race(color, 30,length,i); 
+      race(length,t); 
     
-    i+=1;
-    if(i == strip.numPixels())
-      i = 0; //restart
+      t+=1;
+      if(t == strip.numPixels())
+        t = 0; //restart
+    
    }
   
   
   // if the server's disconnected, stop the client:
+  /*
   if (!client.connected()) {
     Serial.println();
     Serial.println("disconnecting.");
     client.stop();
-  }
+  }*/
   
   
 }
 
 void car(uint32_t c, uint8_t length, uint32_t start) {
-    
-    uint32_t ccolor = strip.getPixelColor(start);
+    /*
+    uint32_t ccolor = strip.getPixelColor(start+1);
     strip.setPixelColor(start,Wheel(ccolor/2)); //Set first and last at different colour
   
     for(uint8_t a=1; a<length-1; a++) {
         strip.setPixelColor(start+a, c);
     }
     strip.setPixelColor(start+length-1,Wheel(ccolor/2));
-    strip.show();
+    */
+    
+    //Draw car
+    for(uint8_t a=0; a<length; a++) {
+        strip.setPixelColor(start+a, c);
+    }
     
 }
 
-// Fill the dots one after the other with a color
-void race(uint32_t c, uint8_t wait, uint8_t length,uint16_t i) {
+
+void race(uint8_t length,uint16_t i) {
   
-      strip.setPixelColor(i,strip.Color(0, 0, 0));
-      car(c,length,i+1);
-      //strip.setPixelColor(i+length,c);
-      
-      strip.show();
+  for(int nr = 0; nr < nrCars; nr++) {
+        
+     if(i % velocity[nr] == 0) { //update position based on velocity
+        currentPos[nr] +=1;
+        strip.setPixelColor(currentPos[nr]-1,strip.Color(0,0,0)); //Clear previous position
+     }
+     
+     car(color[nr],length,currentPos[nr]);
+       
+  }
+  
+  strip.show();
 }
 
 uint32_t Wheel(byte WheelPos) {
